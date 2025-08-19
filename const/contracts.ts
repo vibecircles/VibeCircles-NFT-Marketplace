22ae7fff8c184595e50cf906b13e26a331c0a1a1
VibesCircles <vibecircless@gmail.com>
2025-08-19 02:27:37 +0200

    update

--- const/contracts.ts
+++ const/contracts.ts
@@ -1,29 +1,38 @@
 import client from "@/lib/client";
-/** Replace the values below with the addresses of your smart contracts. */
-
-// 1. Set up the network your smart contracts are deployed to.
-// First, import the chain from the package, then set the NETWORK variable to the chain.
 import { getContract } from "thirdweb";
 import { defineChain } from "thirdweb/chains";
-export const NETWORK = defineChain(199); // BTTC (chainId 199)
 
-// 2. The address of the marketplace V3 smart contract.
-// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+/**
+ * 1. Set up the network your smart contracts are deployed to.
+ * BTTC (BitTorrent Chain) uses chainId 199.
+ */
+export const NETWORK = defineChain(199);
+
+/**
+ * 2. MarketplaceV3 contract
+ * Deploy your own from: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+ */
 const MARKETPLACE_ADDRESS = "0x858d7849196CF3c1b2F5456aCFBede24c3636186";
+
 export const MARKETPLACE = getContract({
-	address: MARKETPLACE_ADDRESS,
-	client,
-	chain: NETWORK,
+  address: MARKETPLACE_ADDRESS,
+  client,
+  chain: NETWORK,
 });
 
-// 3. The address of your NFT collection smart contract.
+/**
+ * 3. NFT Collection contract
+ */
 const NFT_COLLECTION_ADDRESS = "0x55106429E0aAD7007bA00149c14C7D0389811b78";
+
 export const NFT_COLLECTION = getContract({
-	address: NFT_COLLECTION_ADDRESS,
-	client,
-	chain: NETWORK,
+  address: NFT_COLLECTION_ADDRESS,
+  client,
+  chain: NETWORK,
 });
 
-// (Optional) Set up the URL of where users can view transactions on
-// For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
+/**
+ * 4. Block explorer
+ * Use this to link to transaction or contract details.
+ */
 export const ETHERSCAN_URL = "https://bttcscan.com/";
--- lib/client.ts
+++ lib/client.ts
@@ -1,9 +1,13 @@
 import { createThirdwebClient } from "thirdweb";
 
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 
 if (!clientId) {
-  throw new Error("Client ID not set. Please add NEXT_PUBLIC_TEMPLATE_CLIENT_ID to your .env.local file");
+  throw new Error("Client ID not set. Please add NEXT_PUBLIC_THIRDWEB_CLIENT_ID to your .env.local file");
 }
 
-export default createThirdwebClient({ clientId });
+const client = createThirdwebClient({
+  clientId: clientId,
+});
+
+export default client;
